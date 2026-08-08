7ee4cfe47a3b82dd43cd13f14dbf981fd258b5ce
Bayu <bayun791@gmail.com>
2026-08-09 06:42:19 +0700

    Configure Railway Procfile and clean up

--- api/index.php
+++
@@ -1,9 +0,0 @@
-make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-
-$response->send();
-
-$kernel->terminate($request, $response);
